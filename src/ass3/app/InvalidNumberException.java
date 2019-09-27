237848ebab992db98e41218b665386fab03a10ab
Yuxin Chen <yche864@en-344761.uoa.auckland.ac.nz>
2019-09-27 12:10:47 +1200

    adding files to repo

---
+++ src/ass3/app/InvalidNumberException.java
@@ -0,0 +1,10 @@
+package ass3.app;
+
+//This class is to check the invalid input
+public class InvalidNumberException extends RuntimeException {
+
+	public InvalidNumberException(String message) {
+		super(message);
+	}
+
+}
